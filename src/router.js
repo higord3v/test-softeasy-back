1ed575e995bb57417cc41a2d482746a895e21dd6
higor-d3v <hgrprofissional@gmail.com>
2022-03-14 20:13:38 -0300

    swagger

--- src/router.js
+++ src/router.js
@@ -2,6 +2,11 @@ const express = require("express");
 const book = require("./controllers/book");
 const router = express();
 router.use(express.json())
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.post("/book", book.insertBook);
 router.get("/book/:id", book.listBook);
