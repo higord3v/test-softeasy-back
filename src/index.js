0c6bad263e384e96567f2706f9fbe8ad2d90e6ef
higor-d3v <hgrprofissional@gmail.com>
2022-03-14 19:18:20 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = require("./router");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
-app.use(router);
-
+app.use(cors())
+app.use(router)
 app.listen(8000, () => console.log("running on port 8000"));
