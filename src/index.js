ed06a47537861fbf3b58cc93c13487992240462c
higor-d3v <hgrprofissional@gmail.com>
2022-03-19 17:29:40 -0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ const cors = require("cors");
 const app = express();
 app.use(cors())
 app.use(router)
-app.listen(process.env.PORT, () => console.log("running on port 8000"));
+app.listen(process.env.PORT);
