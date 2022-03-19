9496f4003c85a11d21b2dc2cb1f7dcd23368bae2
higor-d3v <hgrprofissional@gmail.com>
2022-03-19 16:30:42 -0300

    .env

--- src/ORM/conexao.js
+++ src/ORM/conexao.js
@@ -1,8 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("bookshop", "postgres", "postgres", {
+const sequelize = new Sequelize({
+  database: process.env.DB,
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
   dialect: "postgres",
-  host: "localhost",
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false
+    }
+  },
 });
 
 module.exports = sequelize;
